df179cc08ce6dde74f48b8d628c039741715fb57
Carmen-Elena Bruma <160878690+carmenelenab@users.noreply.github.com>
2025-07-06 17:18:05 +0000

    Completed the course tasks

---
+++ _02_02/Ticket.java
@@ -0,0 +1,10 @@
+package _02_02;
+
+class Ticket {
+  public Ticket() {}
+
+  private String destination;
+  private double price;
+  private boolean isReturn;
+
+}
--- _03_07b/EnhancedForLoops.java
+++ _03_07b/EnhancedForLoops.java
@@ -8,14 +8,26 @@ public class EnhancedForLoops {
   public static void main(String[] args) {
     int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17, 19 };
     // Write an enhanced for loop to print out each prime number in the array.
-
+    for (int primeNum : primeNumbers) {
+      System.out.print(primeNum + " ");
+    }
+    System.out.println();
     List<String> weekDays = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
     // Write an enhanced for loop to print out each week day in the list.
+    for (String day : weekDays) {
+      System.out.print(day + " ");
+    }
+    System.out.println();
 
     int[] randomNumbers = { 23, 51, 72, 84, 1, 60, 34, 102 };
     // Write an enhanced for loop to print out the numbers in the array that are
     // greater than 50.
-
+    for (int num : randomNumbers) {
+      if (num > 50) {
+        System.out.print(num + " ");
+      }
+    }
+    System.out.println();
   }
 
 }
